012f6d980788df5d56bb222085f982a1836ad205
Jan Skowronski <crianonim@gmail.com>
2019-10-26 11:17:17 +0100

    Add getScenario to parse scenario.txt for game content

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,7 +1,39 @@
 package site.jans.game
+import scala.io.Source
 
 object Game {
-    def main(args: Array[String]):Unit = {
-        println("Welcome to Choose Your Adventure!");
+  def main(args: Array[String]): Unit = {
+    println("Welcome to Choose Your Adventure!");
+    val dialogs: Map[String, Dialog] = getScenario()
+    println(dialogs)
+  }
+
+  def getScenario(): Map[String, Dialog] = {
+    var id: String = ""
+    var text = "";
+    var options = Vector.empty[DialogOption]
+    var listOfDial = List.empty[(String, Dialog)]
+    for (line <- Source.fromFile("scenario.txt").getLines) {
+      if (line.length() == 0) {
+        val dial = Dialog(id, text, options)
+        listOfDial = (id, dial) :: listOfDial
+        id = "";
+        text = "";
+        options = Vector.empty[DialogOption]
+      } else {
+        if (line.startsWith("#")) {
+          id = line.substring(1)
+        } else if (line.startsWith("- ")) {
+          val option = line.substring(2)
+          val arr = option.indexOf("->")
+          val text = option.substring(0, arr - 1)
+          val goTo = option.substring(arr + 4)
+          options = options :+ DialogOption(text, goTo)
+        } else {
+          text += line + "\n"
+        }
+      }
     }
+    listOfDial.toMap
+  }
 }
