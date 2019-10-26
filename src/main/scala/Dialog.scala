74c208c07bdc4d2e26c6550f7a8b32c03c1334d9
Jan Skowronski <crianonim@gmail.com>
2019-10-26 09:58:39 +0100

    Add Dialog and DialogOption classes

---
+++ src/main/scala/Dialog.scala
@@ -0,0 +1,11 @@
+package site.jans.game
+
+case class DialogOption(text:String,goTo:String)
+
+case class Dialog(id:String,title: String, options:Vector[DialogOption]){
+   def display():String={
+    val titleString=title+"\n "
+    val optionsString=options.zipWithIndex map(x=>(x._2+1)+") "+x._1.text) mkString("\n ")
+    titleString+optionsString+"\n"
+  }
+}
