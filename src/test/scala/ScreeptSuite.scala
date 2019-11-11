98541d5319d9a767c2fc6b0dd7f3e16bd2bf9983
Jan Skowronski <crianonim@gmail.com>
2019-11-11 15:51:39 +0000

    Update tests

--- src/test/scala/ScreeptSuite.scala
+++ src/test/scala/ScreeptSuite.scala
@@ -1,6 +1,7 @@
 import org.scalatest.FunSuite
-import site.jans.game.Screept
-
+import site.jans.screept
+import site.jans.screept._
+import scala.collection.mutable
 class ScreeptSuite extends FunSuite {
 
   // just so I know how to use
@@ -21,5 +22,24 @@ class ScreeptSuite extends FunSuite {
     assert(!Screept.toBoolean(null))
     assert(Screept.toBoolean(1))
     assert(Screept.toBoolean("value"))
+  }
+  
+  test("test MathOperators operators"){
+    val eval=Screept.evaluate(MathOperators.operators ++ BasicOperators.operators)(mutable.Map[String,String]()) _
+    assert(eval("3 2 + 5 =")=="1")
+    assert(eval("3 -2 + 1 =")=="1")
+    assert(eval("-3 -2 + -5 =")=="1")
+    assert(eval("-3 -2 - -1 =")=="1")
+    assert(eval("-10 -2 / 5 =")=="1")
+    assert(eval("-10 4 / -2.5 =")=="1")
+    assert(eval("-10 -3 * 30 =")=="1")
+    assert(eval("-10 2 * -20 =")=="1")
+  }
+  test("test BasicOperators operators"){
+    val eval=Screept.evaluate(MathOperators.operators ++ BasicOperators.operators)(mutable.Map[String,String]()) _
+    assert(eval("Jan name := name =")=="1")
+    assert(eval("Jan name := name Jan =")=="1")
+    assert(eval("True False 1 1 = ?")=="True")
+    assert(eval("True False 1 2 = ?")=="False")
   }
 }
