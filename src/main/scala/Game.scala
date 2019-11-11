64d316b6264b2488928283b59755ae34d8da9dc6
Jan Skowronski <crianonim@gmail.com>
2019-11-11 17:34:32 +0000

    Change goTo to run and execute

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -6,20 +6,34 @@ import site.jans.screept._
 
 object Game {
 
-  def init(scenarioText: String, startDialogName: String): Unit = {
-    gameLoop(getScenario(scenarioText), startDialogName)
+  def init(
+      scenarioText: String,
+      startDialogName: String,
+      ctx: mutable.Map[String, String]
+  ): Unit = {
+    gameLoop(getScenario(scenarioText), startDialogName, ctx)
   }
 
-  def gameLoop(dialogs: Map[String, Dialog], dialogName: String): Unit = {
+  def gameLoop(
+      dialogs: Map[String, Dialog],
+      dialogName: String,
+      ctx: mutable.Map[String, String]
+  ): Unit = {
     var current = dialogName;
     var playing = true
     while (playing) {
+      println(ctx)
       println(dialogs.get(current).get.display())
       val option = scala.io.StdIn.readInt()
       if (option == 0) {
         playing = false
       } else {
-        current = dialogs.get(current).get.options(option - 1).goTo
+        val toRun = dialogs.get(current).get.options(option - 1).run
+        val result = Screept.evaluate(
+          LogicOperators.operators ++ BasicOperators.operators ++ MathOperators.operators
+        )(ctx)(toRun)
+        if (result(0) == '#')
+          current = result.tail
       }
     }
   }
@@ -32,14 +46,14 @@ object Game {
 
     val IdExtractor = """^#(.+)""".r
     val EmpyExtractor = """^$""".r
-    val OptionExtractor = """^- (.+) -> #(.+)$""".r
+    val OptionExtractor = """^- (.+) -> (.+)$""".r
 
     // currently built Dialog parts
     var id: String = ""
     var text = "";
     var options = Vector.empty[DialogOption]
 
-    def addDialog():Unit = {
+    def addDialog(): Unit = {
       val dial = Dialog(id, text, options)
       listOfDial = (id, dial) :: listOfDial
       id = "";
@@ -48,10 +62,10 @@ object Game {
     }
     for (line <- scenarioText.split("\n")) {
       line match {
-        case EmpyExtractor() => addDialog()
+        case EmpyExtractor()      => addDialog()
         case IdExtractor(foundId) => id = foundId
-        case OptionExtractor(text, goTo) =>
-          options = options :+ DialogOption(text, goTo)
+        case OptionExtractor(text, run) =>
+          options = options :+ DialogOption(text, run)
         case _ => text += line + "\n"
       }
     }
--- src/main/scala/scenario/Scen1.scala
+++ src/main/scala/scenario/Scen1.scala
@@ -1,10 +1,12 @@
 package scenario
 
 import site.jans.game._
+import scala.collection.mutable
 
 object Scen1 {
 
     def main(args: Array[String]): Unit = {
-        Game.init(Game.readScenarioFile("scenario.txt"),"start")
+        val ctx=mutable.Map("turn"->"1")
+        Game.init(Game.readScenarioFile("scenario.txt"),"start",ctx)
     }
 }
