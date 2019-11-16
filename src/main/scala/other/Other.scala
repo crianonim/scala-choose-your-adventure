2d0c8c01a663c5b23cd87bc7fd9dbf8aedc12a6e
Jan Skowronski <crianonim@gmail.com>
2019-11-16 07:26:00 +0000

    Add getScenarios() to GameServer

--- src/main/scala/other/Other.scala
+++ src/main/scala/other/Other.scala
@@ -1,20 +1,20 @@
 package other
 import site.jans.game._
-import site.jans.screept.MathOperators
-import site.jans.screept.LogicOperators
-import site.jans.screept.BasicOperators
+import site.jans.screept.Screept
 import scala.annotation.varargs
 object Other {
     def main(args: Array[String]):Unit = {
-        val operators=MathOperators.operators++LogicOperators.operators++BasicOperators.operators
+        val operators=Screept.getCoreOperators()
         val initialCtx=Map("dialog"->"start","turn"->"1")
         val s1=Scenario("pierwszy scenariusz",Scenario.readScenarioFile("scenario.txt"),operators,initialCtx)
+        val s2=Scenario("London Life",Scenario.readScenarioFile("scen2.txt"),operators,initialCtx)
+
         // println(s1.dialogs)
         // println(s1.newContext)
         // val p1=Playthrough(s1,s1.newContext)
         // println(p1)
         // println("V",p1.validOptions)
-        val gs=GameServer(List(s1))
+        val gs=GameServer(List(s1,s2))
         val gameId=gs.startGame(0)
         val p1=gs.getGame(gameId)
         // println("SHOW",p1.show())
@@ -24,7 +24,8 @@ object Other {
         // println("SHOW",p1.show())
         // val p2=gs.getGame(gs.startGame(0))
         // println("SHOW",p2.show())
-        gs.cliPlay(p1)
+        println(gs.getScenarios())
+        // gs.cliPlay(p1)
 
     }
 }
--- src/main/scala/site/jans/game/GameServer.scala
+++ src/main/scala/site/jans/game/GameServer.scala
@@ -1,34 +1,44 @@
 package site.jans.game
 import scala.collection.mutable
 
-case class GameServer(scenarios: List[Scenario]){
-   private var id=0
-   private val playthroughs=mutable.Map[Int,Playthrough]()
-    def startGame(scenario_id:Int)={
-        val scenario=scenarios(scenario_id)
-        val newGame=Playthrough(scenario,scenario.newContext)
-        id=id+1
-        playthroughs(id)=newGame
-        id
-    }
-    def getGame(playthrough_id:Int)={
-        playthroughs(playthrough_id)
-    }
-    def cliPlay(playthrough: Playthrough)={
-        var playing=true
-        do {
-            val dialog=playthrough.show();
-            println(dialog._1);
-            println(dialog._2);
-            println(dialog._3.zipWithIndex map(x=>(x._2+1)+") "+x._1) mkString("\n"));
-            println("0) Exit")
+case class GameServer(scenarios: List[Scenario]) {
+  private var id = 0
+  private val playthroughs = mutable.Map[Int, Playthrough]()
+  def startGame(scenario_id: Int) = {
+    val scenario = scenarios(scenario_id)
+    val newGame = Playthrough(scenario, scenario.newContext)
+    id = id + 1
+    playthroughs(id) = newGame
+    id
+  }
+  def getScenarios() = {
+    for {  x <-  scenarios.zipWithIndex
+      val result = x match {
+        case (x,i)=>(i,x.title)
+      }
+    } yield result
+  }
+  
+  def getGame(playthrough_id: Int) = {
+    playthroughs(playthrough_id)
+  }
+  def cliPlay(playthrough: Playthrough) = {
+    var playing = true
+    do {
+      val dialog = playthrough.show();
+      println(dialog._1);
+      println(dialog._2);
+      println(
+        dialog._3.zipWithIndex map (x => (x._2 + 1) + ") " + x._1) mkString ("\n")
+      );
+      println("0) Exit")
 
-         val option = scala.io.StdIn.readInt()
-         if (option == 0) {
-           playing = false
-         } else {
-           playthrough.play(option)
-         }
-        } while (playing==true)
-    }
+      val option = scala.io.StdIn.readInt()
+      if (option == 0) {
+        playing = false
+      } else {
+        playthrough.play(option)
+      }
+    } while (playing == true)
+  }
 }
